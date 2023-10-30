c189f041f7207d170d6ca563ee4c3e621c11a3be
Ricardo Vieira <91328070+VieiraRicardo777@users.noreply.github.com>
2023-10-30 18:43:54 -0300

    correção card component

--- src/app/Shared/card/card.component.ts
+++ src/app/Shared/card/card.component.ts
@@ -6,6 +6,6 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-  @Input() variant: 'primary' | 'secondary' = 'primary'
+  @Input() variant: 'primary' | 'secundary' = 'primary'
 
 }
